77ed697c48d003744be0af0a641cb5cda8b7d587
Rishit Guleria <rishit@example.com>
2026-02-19 02:00:04 +0530

    INTIAL_COMMIT : MONOREPO SETUP

--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,15 +1,13 @@
 import { Button } from "@workspace/ui/components/button"
 import { add } from "@workspace/math/add"
-import { input } from "@workspace/ui/components/input"
 export default function Page() {
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello appsweb</h1>
+        <h1 className="text-2xl font-bold">Hello apps/widget</h1>
         <div className="flex gap-2">
           <Button>Button</Button>
-          <p>{add (3 , 3)}</p>
-          <input />
+          <p>{add (3,3)}</p>
           <Button variant="outline">Outline</Button>
         </div>
       </div>
