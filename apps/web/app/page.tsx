837965f90029b094bec0ab358f7346243d50a87e
Rishit Guleria <rishit@example.com>
2026-02-22 01:05:52 +0530

    added convex(db service)

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,17 +1,16 @@
+'use client'
+import { useQuery  , useMutation } from "convex/react";
+import { api } from "@workspace/backend/convex/_generated/api";
 import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
-import { input } from "@workspace/ui/components/input"
 export default function Page() {
+  const users  = useQuery (api.users.getmany)
+  const addUser = useMutation (api.users.add);
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello appsweb</h1>
-        <div className="flex gap-2">
-          <Button>Button</Button>
-          <p>{add (3 , 3)}</p>
-          <input />
-          <Button variant="outline">Outline</Button>
-        </div>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p> apps/web</p>
+      <Button onClick ={() => addUser()}>Add</Button>
+      <div className="max-w-sm  w-full mx-auto">
+        {JSON.stringify(users , null , 2)} 
       </div>
     </div>
   )
--- apps/widget/app/page.tsx
+++ apps/widget/app/page.tsx
@@ -1,15 +1,14 @@
-import { Button } from "@workspace/ui/components/button"
-import { add } from "@workspace/math/add"
+'use client'
+import { useQuery } from "convex/react";
+import { api } from "@workspace/backend/convex/_generated/api";
+
 export default function Page() {
+  const users  = useQuery (api.users.getmany)
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello apps/widget</h1>
-        <div className="flex gap-2">
-          <Button>Button</Button>
-          <p>{add (3,3)}</p>
-          <Button variant="outline">Outline</Button>
-        </div>
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <p> apps/wi</p>
+      <div className="max-w-sm  w-full mx-auto">
+        {JSON.stringify(users , null , 2)} 
       </div>
     </div>
   )
