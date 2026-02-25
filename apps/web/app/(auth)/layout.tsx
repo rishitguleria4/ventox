eb04a4278e1b1277c2daa8f93215bb1e26146d3b
Rishit Guleria <rishit@example.com>
2026-02-25 15:31:46 +0530

    pull req review try

---
+++ apps/web/app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+const layout = ({ children }: { children: React.ReactNode }) => {
+    return (
+        <div className = " min-h-screen min-w-screen flex-col h-full flex items-center justify-center">
+            {children}
+        </div>
+    );
+}
+
+export default layout;
+// dfnvnefjk
